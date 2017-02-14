aa7ef7838445abaa153acd78ea18f5df79858f3a
chico <chi187@gmail.com>
2017-02-14 14:03:53 +0300

    WS-171.3019.10 <chico@xyzzy.local Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, ide.general.xml, React component.js, project.default.xml, diff.xml, git.xml, other.xml, Default.xml, Monokai Sublime Text 3.icls, file.template.settings.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, code.style.schemes.xml, plugin_ui.xml, colors.scheme.xml, Programmer.icls, notifications.xml, github_settings.xml, Obsidian.icls, Default.xml, JavaScript.xml, laf.xml, Default _1_.xml, web-browsers.xml, abbreviations.xml, Material Theme - Chico.icls, vcs.xml, editor.codeinsight.xml, keymap.xml, Wombat.icls, editor.xml, applicationLibraries.xml, debugger.xml

---
+++ fileTemplates/React component.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class ${NAME} extends Component {
+  static propTypes = {
+    
+  };
+  constructor(...args) {
+    super(...args);
+  }
+  render() {
+    return (
+        
+    );
+  }
+}
